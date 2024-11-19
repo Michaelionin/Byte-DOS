4187b940b25d71d1a539269b256ff1598cd13a7e
Michael <ioninmihail1@gmail.com>
2024-11-19 17:56:19 +0300

    Update MEMCOUNT.cs

--- AcronixOS/Code/CORE/System32/HARDWARE/MEMCOUNT/MEMCOUNT.cs
+++ AcronixOS/Code/CORE/System32/HARDWARE/MEMCOUNT/MEMCOUNT.cs
@@ -9,7 +9,7 @@ namespace AcronixOS.Code.CORE.System32.HARDWARE.MEMCOUNT
         {
             if (CPU.GetAmountOfRAM() + 2 < 64) // Проверка необходимого кол-ва ОЗУ
             {
-                Console.WriteLine("[ERROR] OUT OF MEMORY!\nYour system does not have enough RAM to run AcronixOS!\nAt least 64 Mb of RAM is required, but you only have " + (Cosmos.Core.CPU.GetAmountOfRAM() + 2) + " Mb.");
+                Console.WriteLine("[ERROR] OUT OF MEMORY!\nYour system does not have enough RAM to run Byte-DOS!\nAt least 64 Mb of RAM is required, but you only have " + (Cosmos.Core.CPU.GetAmountOfRAM() + 2) + " Mb.");
                 Console.ReadKey(true);
                 Cosmos.System.Power.Shutdown();
             }
