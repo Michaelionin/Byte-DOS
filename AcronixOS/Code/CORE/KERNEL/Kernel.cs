261242fbeef900ac9150e3a542ad2e54b3e97cbe
Michael <ioninmihail1@gmail.com>
2024-11-19 17:54:57 +0300

    Update Kernel.cs

--- AcronixOS/Code/CORE/KERNEL/Kernel.cs
+++ AcronixOS/Code/CORE/KERNEL/Kernel.cs
@@ -15,7 +15,7 @@ namespace Kernel
         protected override void BeforeRun()
         {
             Console.Clear(); // Очищаем дисплей
-            Console.WriteLine("[INFO] AcronixOS Kernel Version "+ registry32.OS_Version+" loaded.");
+            Console.WriteLine("[INFO] Byte-DOS Kernel Version "+ registry32.OS_Version+" loaded.");
             Console.WriteLine("[INFO] Checking system ram amount..."); 
             MEMCOUNT.Initialize(); // Подсчитываем ОЗУ
             Console.WriteLine("[INFO] Command prompt loading...");
@@ -32,12 +32,9 @@ namespace Kernel
         protected override void Run()
         {
             Console.Clear(); // Очищаем дисплей
-            Console.WriteLine("Welcome to AcronixOS 2023 v" + registry32.OS_Version, Console.ForegroundColor = ConsoleColor.Green);
+            Console.WriteLine("Welcome to Byte-DOS  v" + registry32.OS_Version, Console.ForegroundColor = ConsoleColor.Green);
             Console.Write("To display a list of commands, type: ", Console.ForegroundColor = ConsoleColor.White); Console.WriteLine("HELP", Console.ForegroundColor = ConsoleColor.Green);
-            Console.Write("\nWebsite             :  ", Console.ForegroundColor = ConsoleColor.White); Console.WriteLine("https://FlyConstant.ruok.org/", Console.ForegroundColor = ConsoleColor.Green);
-            Console.Write("AcronixOS Wiki Guide:  ", Console.ForegroundColor = ConsoleColor.White); Console.WriteLine("https://github.com/pie-with-jam/AcronixOS/wiki", Console.ForegroundColor = ConsoleColor.Green);
-            Console.Write("AcronixOS Support   :  ", Console.ForegroundColor = ConsoleColor.White); Console.WriteLine("https://github.com/pie-with-jam/AcronixOS/issues \n", Console.ForegroundColor = ConsoleColor.Green);
-
+            
 
             CommandManager.GetInput(); // Получение строки терминала
         }
